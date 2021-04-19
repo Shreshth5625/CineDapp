a0ce1253f8181efe4f87cf1f0ef57b94cc51ad46
Shreshth Bhatt <shreshthgit56@gmail.com>
2021-04-19 17:17:17 +0530

    bug fix

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -38,9 +38,7 @@ import "./SafeMath.sol";
 
      // Functions with this modifier can only be executed by the owner
      modifier onlyOwner() {
-         require(msg.sender != owner); {
-
-          }
+         require(msg.sender != owner); 
           _;
       }
 
